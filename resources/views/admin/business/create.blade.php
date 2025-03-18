0f63e0b47414b7c4041bad89c7539cd5e2e29ef5
rostomy <rostomyhs@gmail.com>
2025-03-18 20:20:11 +0100

    add business type

--- resources/views/admin/business/create.blade.php
+++ resources/views/admin/business/create.blade.php
@@ -54,6 +54,22 @@
                                     </div>
                                 </div>
 
+
+                                <div class="col-lg-6 mb-2">
+                                    <label>Select Business type</label>
+                                    <div class="gpt-up-down-arrow position-relative">
+                                        <select name="plan_subscribe_id"
+                                                class="form-control table-select w-100 role">
+                                        
+                                                <option value="e-commerce">E-commerce </option>
+                                                <option value="business">Business </option>
+                                                <option value="both">Both</option>
+                                           
+                                        </select>
+                                        <span></span>
+                                    </div>
+                                </div>
+
                                 <div class="col-lg-6 mb-2">
                                     <label>{{ __('Phone') }}</label>
                                     <input type="text" name="phoneNumber" required class="form-control" placeholder="{{ __('Enter Phone Number') }}">
