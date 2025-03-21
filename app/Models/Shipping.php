65a36eb282620bb0d6c331e2295311545c43dbfc
root <root@vmi2508559.contaboserver.net>
2025-03-21 08:12:07 +0100

    fix merage

--- app/Models/Shipping.php
+++ app/Models/Shipping.php
@@ -2,20 +2,18 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-=======
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Enums\BusinessType;
->>>>>>> fa84ff07fb843d5061240878ab37664ebb48c2c6
+
 
 class Shipping extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-=======
+
 
     /**
      * The attributes that are mass assignable.
@@ -40,5 +38,4 @@ class Shipping extends Model
     {
         return $this->belongsTo(ShippingCompanies::class, 'shipping_company_id');
     }
->>>>>>> fa84ff07fb843d5061240878ab37664ebb48c2c6
 }
