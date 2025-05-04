e1e438d9499ef2ece85eec6658b1fc6ca0178313
Moh Bk <123780770+mohbkceo@users.noreply.github.com>
2025-05-04 22:31:57 +0100

    maystro delivery opreations

--- Modules/Business/App/Http/Controllers/AcnooSaleController.php
+++ Modules/Business/App/Http/Controllers/AcnooSaleController.php
@@ -1023,16 +1023,7 @@ class AcnooSaleController extends Controller
                         "product_price" => $sale->totalAmount,
                         "express" => false,
                         "note_to_driver" => "",
-                        "products" => [
-                            [
-
-                                "product_id" => "cd09abfe-54e4-49ed-a3e0-4d330261a90d",
-
-                                "quantity" => 1,
-
-                                "logistical_description" => "test_prodt"
-                            ]
-                        ],
+                        "products" => result,
                     ];
                 }
 
