53a4519c3fdb61c4cf7c651d5005902d2bef613e
petitdavinci6 <petitdavinci6@gmail.com>
2025-05-06 15:18:44 +0100

    fix pop up

--- resources/views/admin/dynamicApiHeader/index.blade.php
+++ resources/views/admin/dynamicApiHeader/index.blade.php
@@ -9,9 +9,10 @@
             <div class="card-bodys">
                 <div class="table-header p-16 d-flex justify-content-between align-items-center">
                     <h4>API Headers</h4>
-                    <a href="{{ route('admin.dynamicApiHeader.create') }}" class="btn btn-primary">
+                    <!-- Trigger the modal -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addApiHeaderModal">
                         <i class="fas fa-plus-circle me-1"></i> Add New API Header
-                    </a>
+                    </button>
                 </div>
                 <div class="responsive-table m-0">
                     <table class="table table-striped table-hover">
@@ -61,6 +62,50 @@
                         {{ $apiHeaders->links('vendor.pagination.bootstrap-5') }}
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Add API Header Modal -->
+<div class="modal fade" id="addApiHeaderModal" tabindex="-1" aria-labelledby="addApiHeaderModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addApiHeaderModalLabel">Add New API Header</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('admin.dynamicApiHeader.store') }}" method="POST" id="createForm">
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-6 mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Enter API Header Name" required>
+                        </div>
+                        <div class="col-lg-6 mb-3">
+                            <label for="api_key" class="form-label">API Key</label>
+                            <input type="text" name="api_key" id="api_key" class="form-control" placeholder="Enter API Key" required>
+                        </div>
+                        <div class="col-lg-6 mb-3">
+                            <label for="status" class="form-label">Status</label>
+                            <select name="status" id="status" class="form-control">
+                                <option value="1">Active</option>
+                                <option value="0">Inactive</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-12 mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea name="description" id="description" class="form-control" rows="4" placeholder="Enter Description"></textarea>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="button-group text-center mt-5">
+                                <button type="reset" class="theme-btn border-btn m-2">{{ __('Cancel') }}</button>
+                                <button type="submit" class="theme-btn m-2 submit-btn">{{ __('Save') }}</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
