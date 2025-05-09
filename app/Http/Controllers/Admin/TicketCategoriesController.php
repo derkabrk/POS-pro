7312f48ba0bc7599222161e981e5fc464746c311
rostomy <rostomyhs@gmail.com>
2025-05-09 22:03:31 +0100

    ticket system

--- app/Http/Controllers/Admin/TicketCategoriesController.php
+++ app/Http/Controllers/Admin/TicketCategoriesController.php
@@ -1,8 +1,9 @@
 <?php
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\TicketCategories;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TicketCategoriesController extends Controller
 {
