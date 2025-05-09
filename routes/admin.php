59b222ca7c9f99bbac4925194bd82a5ba7b88400
rostomy <rostomyhs@gmail.com>
2025-05-09 22:18:33 +0100

    dd

--- routes/admin.php
+++ routes/admin.php
@@ -129,8 +129,7 @@ Route::group(['as' => 'admin.', 'prefix' => 'admin', 'middleware' => ['auth', 'a
     Route::resource('dynamicApiHeader', ADMIN\ApiHeaderController::class)->except(['show',]);
     Route::resource('ticket-system', ADMIN\TicketSystemController::class);
 
-
-    Route::get('/ticket-categories', ADMIN\TicketCategoriesController::class, 'index');
-    Route::post('/ticket-categories', ADMIN\TicketCategoriesController::class, 'store');
+Route::post('/ticket-categories', [ADMIN\TicketCategoriesController::class, 'index'])->name('ticket-categories.index');
+  Route::post('/ticket-categories', [ADMIN\TicketCategoriesController::class, 'store'])->name('ticket-categories.store');
 
 });
