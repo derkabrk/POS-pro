29749da58eac0529e31d000ddb6f129557e8efad
rostomy <rostomyhs@gmail.com>
2025-05-16 12:42:17 +0100

    login interface

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,159 +3,101 @@
 @section('title')
     {{ __('Login') }}
 @endsection
-@section('main_content')
 
-    <!-- auth-page wrapper -->
-    <div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
-        <div class="bg-overlay"></div>
-        <!-- auth-page content -->
-        <div class="auth-page-content overflow-hidden pt-lg-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card overflow-hidden card-bg-fill border-0 card-border-effect-none">
-                            <div class="row g-0">
-                                <div class="col-lg-6">
-                                    <div class="p-lg-5 p-4 auth-one-bg h-100">
-                                        <div class="bg-overlay"></div>
-                                        <div class="position-relative h-100 d-flex flex-column">
-                                            <div class="mb-4">
-                                                <a href="index" class="d-block">
-                                                    <img src="{{ URL::asset('build/images/logo-light.png') }}" alt="" height="18">
-                                                </a>
+@section('content')
+<!-- auth-page wrapper -->
+<div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
+    <div class="bg-overlay"></div>
+    <!-- auth-page content -->
+    <div class="auth-page-content overflow-hidden pt-lg-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card overflow-hidden card-bg-fill border-0 card-border-effect-none">
+                        <div class="row g-0">
+                            <div class="col-lg-6 d-none d-lg-block">
+                                <div class="p-lg-5 p-4 auth-one-bg h-100">
+                                    <div class="bg-overlay"></div>
+                                    <div class="position-relative h-100 d-flex flex-column">
+                                        <div class="mb-4">
+                                            <a href="{{ route('home') }}" class="d-block">
+                                                <img src="{{ asset(get_option('general')['login_page_logo'] ?? 'assets/images/logo/logo.png') }}" alt="" height="32">
+                                            </a>
+                                        </div>
+                                        <div class="mt-auto">
+                                            <div class="mb-3">
+                                                <i class="ri-double-quotes-l display-4 text-success"></i>
                                             </div>
-                                            <div class="mt-auto">
-                                                <div class="mb-3">
-                                                    <i class="ri-double-quotes-l display-4 text-success"></i>
+                                            <div class="carousel-inner text-center text-white pb-5">
+                                                <div class="carousel-item active">
+                                                    <p class="fs-15 fst-italic">" Welcome to {{ config('app.name') }}! "</p>
                                                 </div>
-
-                                                <div id="qoutescarouselIndicators" class="carousel slide"
-                                                    data-bs-ride="carousel">
-                                                    <div class="carousel-indicators">
-                                                        <button type="button" data-bs-target="#qoutescarouselIndicators"
-                                                            data-bs-slide-to="0" class="active" aria-current="true"
-                                                            aria-label="Slide 1"></button>
-                                                        <button type="button" data-bs-target="#qoutescarouselIndicators"
-                                                            data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                                        <button type="button" data-bs-target="#qoutescarouselIndicators"
-                                                            data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                                    </div>
-                                                    <div class="carousel-inner text-center text-white pb-5">
-                                                        <div class="carousel-item active">
-                                                            <p class="fs-15 fst-italic">" Great! Clean code, clean design,
-                                                                easy for customization. Thanks very much! "</p>
-                                                        </div>
-                                                        <div class="carousel-item">
-                                                            <p class="fs-15 fst-italic">" The theme is really great with an
-                                                                amazing customer support."</p>
-                                                        </div>
-                                                        <div class="carousel-item">
-                                                            <p class="fs-15 fst-italic">" Great! Clean code, clean design,
-                                                                easy for customization. Thanks very much! "</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <!-- end carousel -->
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <!-- end col -->
-
-                                <div class="col-lg-6">
-                                    <div class="p-lg-5 p-4">
-                                        <div>
-                                            <h5 class="text-primary">Welcome Back !</h5>
-                                            <p class="text-muted">Sign in to continue to Velzon.</p>
-                                        </div>
-
-                                        <div class="mt-4">
-                                            <form action="index">
-
-                                                <div class="mb-3">
-                                                    <label for="username" class="form-label">Username</label>
-                                                    <input type="text" class="form-control" id="username"
-                                                        placeholder="Enter username">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <div class="float-end">
-                                                        <a href="auth-pass-reset-cover" class="text-muted">Forgot
-                                                            password?</a>
-                                                    </div>
-                                                    <label class="form-label" for="password-input">Password</label>
-                                                    <div class="position-relative auth-pass-inputgroup mb-3">
-                                                        <input type="password" class="form-control pe-5 password-input"
-                                                            placeholder="Enter password" id="password-input">
-                                                        <button
-                                                            class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon"
-                                                            type="button" id="password-addon"><i
-                                                                class="ri-eye-fill align-middle"></i></button>
-                                                    </div>
-                                                </div>
-
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" value=""
-                                                        id="auth-remember-check">
-                                                    <label class="form-check-label" for="auth-remember-check">Remember
-                                                        me</label>
-                                                </div>
-
-                                                <div class="mt-4">
-                                                    <button class="btn btn-primary w-100" type="submit">Sign In</button>
-                                                </div>
-
-                                                <div class="mt-4 text-center">
-                                                    <div class="signin-other-title">
-                                                        <h5 class="fs-13 mb-4 title">Sign In with</h5>
-                                                    </div>
-
-                                                    <div>
-                                                        <button type="button"
-                                                            class="btn btn-primary btn-icon waves-effect waves-light"><i
-                                                                class="ri-facebook-fill fs-16"></i></button>
-                                                        <button type="button"
-                                                            class="btn btn-danger btn-icon waves-effect waves-light"><i
-                                                                class="ri-google-fill fs-16"></i></button>
-                                                        <button type="button"
-                                                            class="btn btn-dark btn-icon waves-effect waves-light"><i
-                                                                class="ri-github-fill fs-16"></i></button>
-                                                        <button type="button"
-                                                            class="btn btn-info btn-icon waves-effect waves-light"><i
-                                                                class="ri-twitter-fill fs-16"></i></button>
-                                                    </div>
-                                                </div>
-
-                                            </form>
-                                        </div>
-
-                                        <div class="mt-5 text-center">
-                                            <p class="mb-0">Don't have an account ? <a
-                                                    href="auth-signup-cover"
-                                                    class="fw-semibold text-primary text-decoration-underline"> Signup</a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- end col -->
                             </div>
-                            <!-- end row -->
+                            <!-- end col -->
+
+                            <div class="col-lg-6">
+                                <div class="p-lg-5 p-4">
+                                    <div class="text-center mb-4">
+                                        <h5 class="text-primary">{{ __('Welcome Back!') }}</h5>
+                                        <p class="text-muted">{{ __('Sign in to continue to') }} {{ config('app.name') }}.</p>
+                                    </div>
+                                    <form method="POST" action="{{ route('login') }}" class="login_form">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label for="email" class="form-label">{{ __('Email') }}</label>
+                                            <input type="email" name="email" class="form-control" id="email" placeholder="{{ __('Enter your Email') }}" required autofocus>
+                                        </div>
+                                        <div class="mb-3">
+                                            <div class="float-end">
+                                                <a href="{{ route('password.request') }}" class="text-muted">{{ __('Forgot password?') }}</a>
+                                            </div>
+                                            <label class="form-label" for="password">{{ __('Password') }}</label>
+                                            <div class="position-relative auth-pass-inputgroup mb-3">
+                                                <input type="password" name="password" class="form-control pe-5 password-input" placeholder="{{ __('Enter password') }}" id="password" required>
+                                                <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                                            </div>
+                                        </div>
+                                        <div class="form-check mb-3">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="auth-remember-check">
+                                            <label class="form-check-label" for="auth-remember-check">{{ __('Remember me') }}</label>
+                                        </div>
+                                        <div class="mt-4">
+                                            <button class="btn btn-primary w-100" type="submit">{{ __('Sign In') }}</button>
+                                        </div>
+                                    </form>
+                                    <div class="mt-4 text-center">
+                                        <p class="mb-0">{{ __("Don't have an account?") }}
+                                            <a href="#" class="fw-semibold text-primary text-decoration-underline" data-bs-toggle="modal" data-bs-target="#registration-modal">{{ __('Signup') }}</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end col -->
                         </div>
-                        <!-- end card -->
+                        <!-- end row -->
                     </div>
-                    <!-- end col -->
-
+                    <!-- end card -->
                 </div>
-                <!-- end row -->
+                <!-- end col -->
             </div>
-            <!-- end container -->
+            <!-- end row -->
         </div>
-        <!-- end auth page content -->
-
+        <!-- end container -->
     </div>
-    <!-- end auth-page-wrapper -->
+    <!-- end auth page content -->
+</div>
+<!-- end auth-page-wrapper -->
 
+<!-- Registration Modal -->
+@include('web.components.signup')
 @endsection
-@section('script')
-    <script src="{{ URL::asset('build/js/pages/password-addon.init.js') }}"></script>
-@endsection
+
+@push('js')
+<script src="{{ asset('assets/js/auth.js') }}"></script>
+@endpush
+
+
