32c942f8562f5aec29ad0ebcc7203d15a62ad2a3
rostomy <rostomyhs@gmail.com>
2025-05-17 18:24:28 +0100

    fix css

--- Modules/Business/resources/views/layouts/partials/css.blade.php
+++ Modules/Business/resources/views/layouts/partials/css.blade.php
@@ -13,7 +13,8 @@
 <!-- Lily -->
 <link rel="stylesheet" href="{{ asset('assets/css/lity.css') }}">
 <!-- Style -->
-
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 <!-- Toaster -->
 <link rel="stylesheet" href="{{ asset('assets/css/toastr.min.css') }}">
 
