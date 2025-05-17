41485cdae33e1940921178c2cad44c5a9efdbed3
rostomy <rostomyhs@gmail.com>
2025-05-17 21:11:20 +0100

    test new ui

--- Modules/Business/App/Http/Controllers/DashboardController.php
+++ Modules/Business/App/Http/Controllers/DashboardController.php
@@ -39,7 +39,7 @@ class DashboardController extends Controller
                                 ->limit(5)
                                 ->get();
 
-        return view('index', compact('stocks', 'purchases', 'sales'));
+        return view('business::index', compact('stocks', 'purchases', 'sales'));
     }
 
     public function getDashboardData()
