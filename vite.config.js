ba6a97bb49918cab052e26bfd93935e1dda14392
rostomy <rostomyhs@gmail.com>
2025-05-19 20:43:26 +0100

    header

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+css: {
+  preprocessorOptions: {
+    scss: {
+      includePaths: [path.resolve(__dirname, 'node_modules')]
+      
+    }
+  }
+},
+  plugins: [
+    laravel({
+      input: [
+        'resources/scss/app.scss',
+        'resources/scss/bootstrap.scss',
+        'resources/scss/icons.scss',
+        'resources/scss/custom.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+   resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            // Add this alias to resolve the missing file
+            'variables-dark': path.resolve(__dirname, 'resources/scss/_variables-dark.scss')
+        }
+    },
 });
