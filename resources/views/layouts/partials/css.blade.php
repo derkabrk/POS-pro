82a079f3cad8ec7b2ad77853ea3d702574b4b3b6
rostomy <rostomyhs@gmail.com>
2025-05-19 16:25:42 +0100

    design refctoring

--- resources/views/layouts/partials/css.blade.php
+++ resources/views/layouts/partials/css.blade.php
@@ -3,7 +3,6 @@
 <link href="https://fonts.googleapis.com/css?family=Saira:400,700&display=swap" rel="stylesheet">
 <!-- Bootstrap -->
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min2.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
 <script src="{{ asset('assets/js/layout.js') }}"></script>
 
@@ -11,13 +10,12 @@
 <!-- Fontawesome -->
 <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome/css/fontawesome-all.min.css') }}">
 {{-- jquery-confirm --}}
-<link rel="stylesheet" href="{{ asset('assets/plugins/jquery-confirm/jquery-confirm.min.css') }}">
+<link rel="stylesheet" href="{{ as set('assets/plugins/jquery-confirm/jquery-confirm.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/summernote-lite.css') }}">
 <!-- Lily -->
 <link rel="stylesheet" href="{{ asset('assets/css/lity.css') }}">
 <!-- Style -->
-<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+
 <!-- Toaster -->
 <link rel="stylesheet" href="{{ asset('assets/css/toastr.min.css') }}">
 
