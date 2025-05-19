2ed424d561c3719006e252fd4baee0d2db3e33ed
rostomy <rostomyhs@gmail.com>
2025-05-19 14:41:57 +0100

    design refctoring

--- resources/views/layouts/partials/css.blade.php
+++ resources/views/layouts/partials/css.blade.php
@@ -2,7 +2,7 @@
 {{-- Google Fonts: Saira --}}
 <link href="https://fonts.googleapis.com/css?family=Saira:400,700&display=swap" rel="stylesheet">
 <!-- Bootstrap -->
-<link rel="stylesheet" href="{{ asset('assets/css/bootstrap2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min2.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
 <script src="{{ asset('assets/js/layout.js') }}"></script>
