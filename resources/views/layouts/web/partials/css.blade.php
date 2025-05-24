b991d925f5740a834daa584e6bd6dc94b9af716c
rostomy <rostomyhs@gmail.com>
2025-05-24 20:02:04 +0100

    updates interfaces

--- resources/views/layouts/web/partials/css.blade.php
+++ resources/views/layouts/web/partials/css.blade.php
@@ -1,22 +1,36 @@
 <link rel="shortcut icon" type="image/x-icon" href="{{ asset(get_option('general')['favicon'] ?? 'assets/images/logo/favicon.png')}}">
-<link rel="stylesheet" href="{{ asset('assets/web/css/bootstrap.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/web/css/swiper-bundle.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/web/fonts/fontawesome/css/all.min.css') }}" />
-<!-- Slick Slider -->
-<link rel="stylesheet" href="{{ asset('assets/web/css/slick.css') }}" />
+{{-- Google Fonts: Saira --}}
+<link href="https://fonts.googleapis.com/css?family=Saira:400,700&display=swap" rel="stylesheet">
+<!-- Bootstrap -->
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min2.css') }}">
+
+<script src="{{ asset('assets/js/layout.js') }}"></script>
+
+
+<!-- Fontawesome -->
+<link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome/css/fontawesome-all.min.css') }}">
 {{-- jquery-confirm --}}
 <link rel="stylesheet" href="{{ asset('assets/plugins/jquery-confirm/jquery-confirm.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/web/css/slick-theme.css') }}" />
-<!-- Custom Css -->
-<link rel="stylesheet" href="{{ asset('assets/web/css/styles.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/web/css/responsive.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/css/summernote-lite.css') }}">
+<!-- Lily -->
+<link rel="stylesheet" href="{{ asset('assets/css/lity.css') }}">
+<!-- Style -->
 
 <!-- Toaster -->
 <link rel="stylesheet" href="{{ asset('assets/css/toastr.min.css') }}">
 
+<link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}">
+
+
+
+<link rel="stylesheet" href="{{ asset('assets/css/icons.min.css') }}">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+<link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+@stack('css')
+
 @if (app()->getLocale() == 'ar')
-<link rel="stylesheet" href="{{ asset('assets/web/css/arabic.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/arabic.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.rtl.min.css') }}">
 @endif
-
-@stack('css')
--- resources/views/layouts/web/partials/footer.blade.php
+++ resources/views/layouts/web/partials/footer.blade.php
@@ -1,122 +1,81 @@
 {{-- Footer Code Start --}}
-<footer class="footer-section py-3">
+<footer class="footer-section py-4 bg-dark text-white">
     <div class="container">
-        <div class="row mt-5">
+        <div class="row mt-5 gy-4">
             <div class="col-md-6 col-lg-5">
                 <a href="{{ route('home') }}">
-                    <img src="{{ asset($general->value['footer_logo'] ?? '') }}" alt="footer-logo" class="footer-logo" />
+                    <img src="{{ asset($general->value['footer_logo'] ?? '') }}" alt="footer-logo" class="footer-logo mb-3" />
                 </a>
-                <p class="mt-3 footer-details">
+                <p class="mb-3 small opacity-75">
                     {{ $page_data['headings']['footer_short_title'] ?? '' }}
                 </p>
-
-                <div class="banner-scan">
-                    @if (!empty($page_data['footer_scanner_image']))
-                    <img src="{{ asset($page_data['footer_scanner_image'] ?? '') }}" alt="" class="w-20" />
-                    <p>
-                        {{ $page_data['headings']['footer_scanner_title'] ?? '' }}
-                    </p>
-                    @endif
+                @if (!empty($page_data['footer_scanner_image']))
+                <div class="d-flex align-items-center gap-2 mb-3">
+                    <img src="{{ asset($page_data['footer_scanner_image'] ?? '') }}" alt="" class="rounded-2 shadow-sm bg-white p-1" style="width: 48px; height: 48px; object-fit: contain;" />
+                    <span class="small opacity-75">{{ $page_data['headings']['footer_scanner_title'] ?? '' }}</span>
                 </div>
-
-                <div class="footer-play-store mt-3 mb-3">
+                @endif
+                <div class="d-flex gap-2 mb-3">
                     @if (!empty($page_data['footer_google_app_image']))
                         <a href="{{ $page_data['headings']['footer_google_play_app_link'] ?? '' }}" target="_blank">
-                            <img src="{{ asset($page_data['footer_google_app_image']) }}" alt="Google Play App" />
+                            <img src="{{ asset($page_data['footer_google_app_image']) }}" alt="Google Play App" class="rounded-2 shadow-sm" style="max-width: 100px;" />
                         </a>
                     @endif
-
                     @if (!empty($page_data['footer_apple_app_image']))
                         <a href="{{ $page_data['headings']['footer_apple_app_link'] ?? '' }}" target="_blank">
-                            <img src="{{ asset($page_data['footer_apple_app_image']) }}" alt="Apple App Store" />
+                            <img src="{{ asset($page_data['footer_apple_app_image']) }}" alt="Apple App Store" class="rounded-2 shadow-sm" style="max-width: 100px;" />
                         </a>
                     @endif
                 </div>
-
             </div>
             <div class="col-md-6 col-lg-4">
-                <h6 class="mb-4 mt-4 mt-md-0 text-white">{{ $page_data['headings']['middle_footer_title'] ?? '' }}</h6>
-                <ul class="d-flex gap-60">
-                    <div class="first-list d-flex flex-column gap-3">
-                        <li>
-                            <a href="{{ $page_data['headings']['right_footer_link_one'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_one'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['right_footer_link_two'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_two'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['right_footer_link_three'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_three'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['right_footer_link_four'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_four'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['right_footer_link_five'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_five'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['right_footer_link_six'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_six'] ?? '' }}</a>
-                        </li>
+                <h6 class="mb-4 mt-4 mt-md-0 text-uppercase fw-bold text-white">{{ $page_data['headings']['middle_footer_title'] ?? '' }}</h6>
+                <div class="row">
+                    <div class="col-6">
+                        <ul class="list-unstyled mb-0">
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['right_footer_link_one'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_one'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['right_footer_link_two'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_two'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['right_footer_link_three'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_three'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['right_footer_link_four'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_four'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['right_footer_link_five'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_five'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['right_footer_link_six'] ?? '' }}" target="_blank">{{ $page_data['headings']['right_footer_six'] ?? '' }}</a></li>
+                        </ul>
                     </div>
-
-                    <div class="second-list d-flex flex-column gap-3">
-                        <li>
-                            <a href="{{ $page_data['headings']['middle_footer_link_one'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_one'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['middle_footer_link_two'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_two'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['middle_footer_link_three'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_three'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['middle_footer_link_four'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_four'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['middle_footer_link_five'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_five'] ?? '' }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ $page_data['headings']['middle_footer_link_six'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_six'] ?? '' }}</a>
-                        </li>
-
+                    <div class="col-6">
+                        <ul class="list-unstyled mb-0">
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['middle_footer_link_one'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_one'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['middle_footer_link_two'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_two'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['middle_footer_link_three'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_three'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['middle_footer_link_four'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_four'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['middle_footer_link_five'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_five'] ?? '' }}</a></li>
+                            <li><a class="text-white-50 text-decoration-none" href="{{ $page_data['headings']['middle_footer_link_six'] ?? '' }}" target="_blank">{{ $page_data['headings']['middle_footer_six'] ?? '' }}</a></li>
+                        </ul>
                     </div>
-                </ul>
+                </div>
             </div>
             <div class="col-md-6 col-lg-3">
-                <h6 class="mb-4 text-white">{{ $page_data['headings']['right_footer_title'] ?? '' }}</h6>
-                <ul class="d-flex flex-column gap-3">
-                    <li>
-                        <a href="{{url($page_data['headings']['left_footer_link_one'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_one'] ?? '' }}</a>
-                    </li>
-                    <li>
-                        <a href="{{url($page_data['headings']['left_footer_link_two'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_two'] ?? '' }}</a>
-                    </li>
-                    <li>
-                        <a href="{{url($page_data['headings']['left_footer_link_three'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_three'] ?? '' }}</a>
-                    </li>
-                    <li>
-                        <a href="{{url($page_data['headings']['left_footer_link_four'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_four'] ?? '' }}</a>
-                    </li>
-                    <li>
-                        <a href="{{url($page_data['headings']['left_footer_link_five'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_five'] ?? '' }}</a>
-                    </li>
+                <h6 class="mb-4 text-uppercase fw-bold text-white">{{ $page_data['headings']['right_footer_title'] ?? '' }}</h6>
+                <ul class="list-unstyled mb-3">
+                    <li><a class="text-white-50 text-decoration-none" href="{{url($page_data['headings']['left_footer_link_one'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_one'] ?? '' }}</a></li>
+                    <li><a class="text-white-50 text-decoration-none" href="{{url($page_data['headings']['left_footer_link_two'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_two'] ?? '' }}</a></li>
+                    <li><a class="text-white-50 text-decoration-none" href="{{url($page_data['headings']['left_footer_link_three'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_three'] ?? '' }}</a></li>
+                    <li><a class="text-white-50 text-decoration-none" href="{{url($page_data['headings']['left_footer_link_four'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_four'] ?? '' }}</a></li>
+                    <li><a class="text-white-50 text-decoration-none" href="{{url($page_data['headings']['left_footer_link_five'] ?? '')}}" target="_blank">{{ $page_data['headings']['left_footer_five'] ?? '' }}</a></li>
                 </ul>
-
-                <ul class="social-link">
+                <ul class="list-inline mb-0">
                     @foreach ($page_data['headings']['footer_socials_links'] ?? [] as $key => $footer_socials_link)
-                        <li>
-                            <a href="{{ url($footer_socials_link) }}" target="_blank">
-                                <img src="{{ asset($page_data['footer_socials_icons'][$key] ?? 'assets/img/demo-img.png') }}" alt="icon">
+                        <li class="list-inline-item me-2">
+                            <a href="{{ url($footer_socials_link) }}" target="_blank" class="d-inline-block bg-white rounded-circle p-1 shadow-sm">
+                                <img src="{{ asset($page_data['footer_socials_icons'][$key] ?? 'assets/img/demo-img.png') }}" alt="icon" style="width: 28px; height: 28px; object-fit: contain;" />
                             </a>
                         </li>
                     @endforeach
                 </ul>
-
-
             </div>
         </div>
-        <hr class="custom-clr-white" />
+        <hr class="border-light opacity-25 my-4" />
         <div class="text-center">
-            <p class="text-white mb-0">{{ $general->value['copy_right'] ?? '' }}</p>
+            <p class="mb-0 small opacity-75">{{ $general->value['copy_right'] ?? '' }}</p>
         </div>
     </div>
 </footer>
