1134b28c57909b86cfefafc0d68567a1c47ab604
rostomy <rostomyhs@gmail.com>
2025-05-25 16:37:44 +0100

    fix categories table

--- Modules/Business/resources/views/categories/index.blade.php
+++ Modules/Business/resources/views/categories/index.blade.php
@@ -7,71 +7,89 @@
 @section('content')
     <div class="admin-table-section">
         <div class="container-fluid">
-            <div class="card bg-light">
-                <div class="card-body">
-                    <div class="table-header d-flex justify-content-between align-items-center p-3">
-                        <h4 class="mb-0">{{ __('Category List') }}</h4>
-                        <a type="button" href="#category-create-modal"
-                            class="btn btn-primary rounded-pill {{ Route::is('admin.categories.create') ? 'active' : '' }}"
-                            data-bs-toggle="modal"><i
-                            class="fas fa-plus-circle me-1"></i>{{ __('Add new Category') }}</a>
-                    </div>
-                    <div class="table-top-form p-3">
-                        <form action="{{ route('business.categories.filter') }}" method="post" class="filter-form"
-                            table="#business-category-data">
-                            @csrf
-                            <div class="d-flex gap-3">
-                                <div class="form-group">
-                                    <select name="per_page" class="form-select">
-                                        <option value="10">{{ __('Show- 10') }}</option>
-                                        <option value="25">{{ __('Show- 25') }}</option>
-                                        <option value="50">{{ __('Show- 50') }}</option>
-                                        <option value="100">{{ __('Show- 100') }}</option>
-                                    </select>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card" id="categoryList">
+                        <div class="card-header border-0">
+                            <div class="row align-items-center gy-3">
+                                <div class="col-sm">
+                                    <h5 class="card-title mb-0">{{ __('Category List') }}</h5>
                                 </div>
-                                <div class="form-group position-relative">
-                                    <input type="text" name="search" class="form-control"
-                                        placeholder="{{ __('Search...') }}">
-                                    <span class="position-absolute top-50 translate-middle-y end-0 me-3">
-                                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M14.582 14.582L18.332 18.332" stroke="#4D4D4D" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M16.668 9.16797C16.668 5.02584 13.3101 1.66797 9.16797 1.66797C5.02584 1.66797 1.66797 5.02584 1.66797 9.16797C1.66797 13.3101 5.02584 16.668 9.16797 16.668C13.3101 16.668 16.668 13.3101 16.668 9.16797Z" stroke="#4D4D4D" stroke-width="1.25" stroke-linejoin="round"/>
-                                        </svg>
-                                    </span>
+                                <div class="col-sm-auto">
+                                    <div class="d-flex gap-1 flex-wrap">
+                                        <a type="button" href="#category-create-modal" data-bs-toggle="modal"
+                                            class="btn btn-primary add-btn"><i
+                                            class="ri-add-line align-bottom me-1"></i> {{ __('Add new Category') }}</a>
+                                    </div>
                                 </div>
                             </div>
-                        </form>
+                        </div>
+                        <div class="card-body border border-dashed border-end-0 border-start-0">
+                            <form action="{{ route('business.categories.filter') }}" method="post" class="filter-form"
+                                table="#business-category-data" id="category-search-form">
+                                @csrf
+                                <div class="row g-3">
+                                    <div class="col-xxl-3 col-sm-6">
+                                        <div class="search-box">
+                                            <input type="text" class="form-control search" name="search"
+                                                value="{{ request('search') }}" placeholder="{{ __('Search...') }}"
+                                                id="category-search-input">
+                                            <i class="ri-search-line search-icon"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-2 col-sm-6">
+                                        <div>
+                                            <select class="form-control" name="per_page" id="category_per_page">
+                                                <option value="10" {{ request('per_page') == 10 ? 'selected' : '' }}>
+                                                    {{__('Show- 10')}}
+                                                </option>
+                                                <option value="25" {{ request('per_page') == 25 ? 'selected' : '' }}>
+                                                    {{__('Show- 25')}}
+                                                </option>
+                                                <option value="50" {{ request('per_page') == 50 ? 'selected' : '' }}>
+                                                    {{__('Show- 50')}}
+                                                </option>
+                                                <option value="100" {{ request('per_page') == 100 ? 'selected' : '' }}>
+                                                    {{__('Show- 100')}}
+                                                </option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-2 col-sm-4">
+                                        <div>
+                                            <!-- Removed filter/search button for real-time search -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="table-responsive table-card" style="margin-top:20px;">
+                                <table class="table table-nowrap mb-0" id="categoryTable">
+                                    <thead class="table-light">
+                                        <tr class="text-uppercase">
+                                            <th style="width: 25px;">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input select-all-delete multi-delete">
+                                                </div>
+                                            </th>
+                                            <th>{{ __('SL') }}.</th>
+                                            <th>{{ __('Icon') }}</th>
+                                            <th class="text-start">{{ __('Name') }}</th>
+                                            <th>{{ __('Status') }}</th>
+                                            <th>{{ __('Action') }}</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="business-category-data">
+                                        @include('business::categories.datas')
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="mt-3">
+                                {{ $categories->links('vendor.pagination.bootstrap-5') }}
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div class="delete-item d-none">
-                    <div class="d-flex justify-content-between align-items-center p-3">
-                        <p class="fw-bold mb-0"><span class="selected-count"></span> {{ __('items selected') }}</p>
-                        <button data-bs-toggle="modal" class="btn btn-danger" data-bs-target="#multi-delete-modal" data-url="{{ route('business.categories.delete-all') }}">{{ __('Delete') }}</button>
-                    </div>
-                </div>
-
-                <div class="table-responsive table-card">
-                    <table class="table table-hover table-nowrap mb-0 align-middle table-bordered" id="datatable">
-                        <thead class="table-light">
-                            <tr>
-                                <th class="text-center">
-                                    <input type="checkbox" class="form-check-input select-all-delete multi-delete">
-                                </th>
-                                <th>{{ __('SL') }}.</th>
-                                <th>{{ __('Icon') }}</th>
-                                <th class="text-start">{{ __('Name') }}</th>
-                                <th>{{ __('Status') }}</th>
-                                <th>{{ __('Action') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody id="business-category-data">
-                            @include('business::categories.datas')
-                        </tbody>
-                    </table>
-                </div>
-                <div class="mt-3">
-                    {{ $categories->links('vendor.pagination.bootstrap-5') }}
                 </div>
             </div>
         </div>
