fb61efc9bc5afcbe53130a96cb28415ecca13b0a
rostomy <rostomyhs@gmail.com>
2025-05-25 15:11:43 +0100

    spacing brands

--- Modules/Business/resources/views/brands/index.blade.php
+++ Modules/Business/resources/views/brands/index.blade.php
@@ -15,35 +15,35 @@
                     </div>
                     <div class="col-sm-auto">
                         <div class="d-flex gap-1 flex-wrap">
-                            <a type="button" href="#brand-create-modal" data-bs-toggle="modal" class="btn btn-primary add-btn"><i class="ri-add-line align-bottom me-1"></i> {{ __('Add new Brand') }}</a>
+                            <a type="button" href="{{ route('business.brands.create') }}" class="btn btn-primary add-btn"><i class="ri-add-line align-bottom me-1"></i> {{ __('Add new Brand') }}</a>
                             <button class="btn btn-soft-danger" id="remove-actions" onClick="deleteMultiple()"><i class="ri-delete-bin-2-line"></i></button>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="card-body border border-dashed border-end-0 border-start-0">
-                <form action="{{ route('business.brands.filter') }}" method="post" class="filter-form" table="#business-brand-data">
+                <form action="{{ route('business.brands.index') }}" method="get" class="filter-form" table="#business-brand-data">
                     @csrf
                     <div class="row g-3">
                         <div class="col-xxl-3 col-sm-6">
                             <div class="search-box">
-                                <input type="text" class="form-control search" name="search" placeholder="{{ __('Search...') }}">
+                                <input type="text" class="form-control search" name="search" value="{{ request('search') }}" placeholder="{{ __('Search...') }}">
                                 <i class="ri-search-line search-icon"></i>
                             </div>
                         </div>
                         <div class="col-xxl-2 col-sm-6">
                             <div>
                                 <select class="form-control" name="per_page" id="per_page">
-                                    <option value="10">{{__('Show- 10')}}</option>
-                                    <option value="25">{{__('Show- 25')}}</option>
-                                    <option value="50">{{__('Show- 50')}}</option>
-                                    <option value="100">{{__('Show- 100')}}</option>
+                                    <option value="10" {{ request('per_page') == 10 ? 'selected' : '' }}>{{__('Show- 10')}}</option>
+                                    <option value="25" {{ request('per_page') == 25 ? 'selected' : '' }}>{{__('Show- 25')}}</option>
+                                    <option value="50" {{ request('per_page') == 50 ? 'selected' : '' }}>{{__('Show- 50')}}</option>
+                                    <option value="100" {{ request('per_page') == 100 ? 'selected' : '' }}>{{__('Show- 100')}}</option>
                                 </select>
                             </div>
                         </div>
                         <div class="col-xxl-2 col-sm-4">
                             <div>
-                                <button type="button" class="btn btn-primary w-100" onclick="SearchData();"> <i class="ri-equalizer-fill me-1 align-bottom"></i>
+                                <button type="submit" class="btn btn-primary w-100"> <i class="ri-equalizer-fill me-1 align-bottom"></i>
                                     {{ __('Filters') }}
                                 </button>
                             </div>
