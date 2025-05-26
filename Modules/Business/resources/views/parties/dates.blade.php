187a4a4a32b25c4f6f3befd221acdc04eafb5e62
rostomy <rostomyhs@gmail.com>
2025-05-26 13:03:08 +0100

    parties  returns table

--- Modules/Business/resources/views/parties/dates.blade.php
+++ Modules/Business/resources/views/parties/dates.blade.php
@@ -1,45 +1,42 @@
 @extends('business::layouts.master')
 
 @section('title')
-    {{ request('type') !== 'Supplier' ? __('Customer List') : __('Supplier List') }}
+    {{ __('Party Dates') }}
 @endsection
 
 @section('content')
     <div class="admin-table-section">
         <div class="container-fluid">
-            <div class="card" id="partyList">
+            <div class="card" id="partyDates">
                 <div class="card-header border-0">
                     <div class="row align-items-center gy-3">
                         <div class="col-sm">
-                            <h5 class="card-title mb-0">{{ request('type') !== 'Supplier' ? __('Customer List') : __('Supplier List') }}</h5>
+                            <h5 class="card-title mb-0">{{ __('Party Dates') }}</h5>
                         </div>
                         <div class="col-sm-auto">
                             <div class="d-flex gap-1 flex-wrap">
-                                <a type="button" href="{{ route('business.parties.create', ['type' => request('type')]) }}"
-                                   class="btn btn-primary add-btn {{ Route::is('business.parties.create') ? 'active' : '' }}">
-                                    <i class="ri-add-line align-bottom me-1"></i>{{ __('Add new ') . ucfirst(request('type')) }}
-                                </a>
+                                <a href="#" class="btn btn-primary add-btn"><i class="ri-add-line align-bottom me-1"></i> {{ __('Add new Date') }}</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="card-body border border-dashed border-end-0 border-start-0">
-                    <form action="{{ route('business.parties.filter', ['type' => request('type')]) }}" method="post" class="filter-form d-flex align-items-center gap-3" table="#parties-data">
+                    <form action="#" method="post" class="filter-form d-flex align-items-center gap-3" table="#party-date-data" id="party-date-search-form">
                         @csrf
                         <div class="row g-3 w-100">
                             <div class="col-xxl-3 col-sm-6">
                                 <div class="search-box">
-                                    <input class="form-control search" type="text" name="search" placeholder="{{ __('Search...') }}" value="{{ request('search') }}">
+                                    <input class="form-control search" type="text" name="search" placeholder="{{ __('Search...') }}" value="{{ request('search') }}" id="party-date-search-input">
                                     <i class="ri-search-line search-icon"></i>
                                 </div>
                             </div>
                             <div class="col-xxl-2 col-sm-6">
                                 <div>
-                                    <select class="form-control" name="per_page">
-                                        <option value="10">{{ __('Show- 10') }}</option>
-                                        <option value="25">{{ __('Show- 25') }}</option>
-                                        <option value="50">{{ __('Show- 50') }}</option>
-                                        <option value="100">{{ __('Show- 100') }}</option>
+                                    <select class="form-control" name="per_page" id="party_date_per_page">
+                                        <option value="10" {{ request('per_page') == 10 ? 'selected' : '' }}>{{__('Show- 10')}}</option>
+                                        <option value="25" {{ request('per_page') == 25 ? 'selected' : '' }}>{{__('Show- 25')}}</option>
+                                        <option value="50" {{ request('per_page') == 50 ? 'selected' : '' }}>{{__('Show- 50')}}</option>
+                                        <option value="100" {{ request('per_page') == 100 ? 'selected' : '' }}>{{__('Show- 100')}}</option>
                                     </select>
                                 </div>
                             </div>
@@ -64,7 +61,7 @@
                         </a>
                     </div>
                     <div class="table-responsive table-card" style="margin-top:20px;">
-                        <table class="table table-nowrap mb-0" id="partiesTable">
+                        <table class="table table-nowrap mb-0" id="partyDateTable">
                             <thead class="table-light">
                                 <tr class="text-uppercase">
                                     <th class="w-60 d-print-none">
@@ -74,21 +71,22 @@
                                     </th>
                                     <th> {{ __('SL') }}. </th>
                                     <th> {{ __('Image') }} </th>
-                                    <th> {{ __('Name') }} </th>
-                                    <th> {{ __('Email') }} </th>
+                                    <th> {{ __('Party Name') }} </th>
+                                    <th> {{ __('Date') }} </th>
                                     <th> {{ __('Type') }} </th>
-                                    <th> {{ __('Phone') }} </th>
-                                    <th> {{ __('Due') }} </th>
+                                    <th> {{ __('Contact') }} </th>
+                                    <th> {{ __('Address') }} </th>
+                                    <th> {{ __('Balance') }}</th>
                                     <th> {{ __('Action') }} </th>
                                 </tr>
                             </thead>
-                            <tbody id="parties-data">
-                                @include('business::parties.datas')
+                            <tbody id="party-date-data">
+                                @include('business::parties.dates-data')
                             </tbody>
                         </table>
                     </div>
                     <div class="mt-3">
-                        {{ $parties->links('vendor.pagination.bootstrap-5') }}
+                        {{-- Pagination links here --}}
                     </div>
                 </div>
             </div>
@@ -100,4 +98,3 @@
     @include('business::component.delete-modal')
     @include('business::parties.view')
 @endpush
-
