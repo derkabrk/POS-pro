8ccdc6fffd09d313c0ee9a533335d758e4d34043
rostomy <rostomyhs@gmail.com>
2025-05-26 21:27:22 +0100

    notifcation ui update

--- Modules/Business/resources/views/notifications/index.blade.php
+++ Modules/Business/resources/views/notifications/index.blade.php
@@ -5,26 +5,52 @@
 @endsection
 
 @section('content')
-    <div class="erp-table-section">
-        <div class="container-fluid">
-            <div class="card card bg-transparent">
-                <div class="card-bodys ">
-                    <div class="table-header p-16">
-                        <h4>{{ __('Notifications List') }}</h4>
+<div class="admin-table-section">
+    <div class="container-fluid">
+        <div class="card" id="notificationList">
+            <div class="card-header border-0">
+                <div class="row align-items-center gy-3">
+                    <div class="col-sm">
+                        <h5 class="card-title mb-0">{{ __('Notifications List') }}</h5>
                     </div>
-                    <div class="table-top-form p-16-0">
+                    <div class="col-sm-auto">
+                        <div class="d-flex gap-1 flex-wrap">
+                            <!-- Add action buttons here if needed -->
+                        </div>
                     </div>
                 </div>
-
-                <div class="table-responsive table-card m-0">
-                    <table class="table table-nowrap mb-0 table-centered align-middle" id="erp-table">
+            </div>
+            <div class="card-body border border-dashed border-end-0 border-start-0">
+                <form action="{{ route('business.notifications.filter') }}" method="post" class="filter-form d-flex align-items-center gap-3" table="#notifications-data">
+                    @csrf
+                    <div class="row g-3 w-100">
+                        <div class="col-xxl-2 col-sm-6">
+                            <select name="per_page" class="form-control">
+                                <option value="10">{{__('Show- 10')}}</option>
+                                <option value="25">{{__('Show- 25')}}</option>
+                                <option value="50">{{__('Show- 50')}}</option>
+                                <option value="100">{{__('Show- 100')}}</option>
+                            </select>
+                        </div>
+                        <div class="col-xxl-3 col-sm-6">
+                            <div class="search-box">
+                                <input class="form-control searchInput" type="text" name="search" placeholder="{{ __('Search...') }}" value="{{ request('search') }}">
+                                <i class="ri-search-line search-icon"></i>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="card-body pt-0">
+                <div class="table-responsive table-card" style="margin-top:20px;">
+                    <table class="table table-nowrap mb-0" id="notificationTable">
                         <thead class="table-light">
                             <tr class="text-uppercase">
-                                <th scope="col">@lang('SL.')</th>
-                                <th scope="col">@lang('Message')</th>
-                                <th scope="col">@lang('Created At')</th>
-                                <th scope="col">@lang('Read At')</th>
-                                <th scope="col">@lang('Action')</th>
+                                <th>{{ __('SL') }}.</th>
+                                <th class="text-start">{{ __('Message') }}</th>
+                                <th class="text-start">{{ __('Created At') }}</th>
+                                <th class="text-start">{{ __('Read At') }}</th>
+                                <th class="text-start">{{ __('Action') }}</th>
                             </tr>
                         </thead>
                         <tbody id="notifications-data" class="searchResults">
@@ -32,9 +58,13 @@
                         </tbody>
                     </table>
                 </div>
+                <div class="mt-3">
+                    {{ $notifications->links('vendor.pagination.bootstrap-5') }}
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @push('modal')
