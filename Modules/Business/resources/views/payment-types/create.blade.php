ea2d42814d1065e57bbe1efdef8613895071eff4
rostomy <rostomyhs@gmail.com>
2025-05-26 19:56:13 +0100

    payment types

--- Modules/Business/resources/views/payment-types/create.blade.php
+++ Modules/Business/resources/views/payment-types/create.blade.php
@@ -1,42 +1,31 @@
-
 <div class="modal fade" id="payment-types-create-modal">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5">{{ __('Create Payment Type') }}</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-content rounded-4 shadow-lg border-0">
+            <div class="modal-header bg-primary text-white rounded-top-4">
+                <h4 class="modal-title fw-bold mb-0">{{ __('Create Payment Type') }}</h4>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div class="personal-info">
-                    <form action="{{ route('business.payment-types.store') }}" method="post" enctype="multipart/form-data"
-                        class="ajaxform_instant_reload">
-                        @csrf
-
-                        <div class="row">
-                            <div class="col-lg-12 mb-2">
-                                <label>{{ __('Name') }}</label>
-                                <input type="text" name="name" required class="form-control" placeholder="{{ __('Enter Name') }}">
-                            </div>
-
-                            <div class="col-lg-12 mb-2">
-                                <label>{{ __('Status') }}</label>
-                                <div class="gpt-up-down-arrow position-relative">
-                                    <select name="status" required class="form-control select-dropdown">
-                                        <option value="1">{{ __('Active') }}</option>
-                                        <option value="0">{{ __('Deactive') }}</option>
-                                    </select>
-                                    <span></span>
-                                </div>
-                            </div>
-                         </div>
-                        <div class="col-lg-12">
-                            <div class="button-group text-center mt-3">
-                                <button type="reset" class="theme-btn border-btn m-2">{{ __('Reset') }}</button>
-                                <button class="theme-btn m-2 submit-btn">{{ __('Save') }}</button>
-                            </div>
+            <div class="modal-body p-4">
+                <form action="{{ route('business.payment-types.store') }}" method="post" enctype="multipart/form-data" class="ajaxform_instant_reload">
+                    @csrf
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <label class="form-label fw-semibold">{{ __('Name') }}</label>
+                            <input type="text" name="name" required class="form-control form-control-lg" placeholder="{{ __('Enter Name') }}">
                         </div>
-                    </form>
-                </div>
+                        <div class="col-12">
+                            <label class="form-label fw-semibold">{{ __('Status') }}</label>
+                            <select name="status" required class="form-select form-select-lg">
+                                <option value="1">{{ __('Active') }}</option>
+                                <option value="0">{{ __('Deactive') }}</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end gap-2 mt-4">
+                        <button type="reset" class="btn btn-outline-secondary rounded-pill px-4">{{ __('Reset') }}</button>
+                        <button class="btn btn-primary rounded-pill px-4 submit-btn"><i class="ri-save-3-line me-1"></i> {{ __('Save') }}</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- Modules/Business/resources/views/payment-types/edit.blade.php
+++ Modules/Business/resources/views/payment-types/edit.blade.php
@@ -1,43 +1,36 @@
-
 <div class="modal fade" id="payment-types-edit-modal">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5">{{ __('Edit Payment Type') }}</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-content rounded-4 shadow-lg border-0">
+            <div class="modal-header bg-primary text-white rounded-top-4">
+                <h4 class="modal-title fw-bold mb-0">{{ __('Edit Payment Type') }}</h4>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div class="personal-info">
-                    <form action="" method="post" enctype="multipart/form-data"
-                        class="ajaxform_instant_reload paymentTypeUpdateForm">
-                        @csrf
-                        @method('put')
-                        <div class="row">
-                            <div class="col-lg-12 mb-2">
-                                <label>{{ __('Name') }}</label>
-                                <input type="text" name="name" id="PaymentTypeName" required class="form-control" placeholder="{{ __('Enter Name') }}">
-                            </div>
-
-                            <div class="col-lg-12 mb-2">
-                                <label>{{ __('Status') }}</label>
-                                <div class="gpt-up-down-arrow position-relative">
-                                    <select name="status" id="PaymentTypeStatus" required class="form-control select-dropdown">
-                                        <option value="1">{{ __('Active') }}</option>
-                                        <option value="0">{{ __('Deactive') }}</option>
-                                    </select>
-                                    <span></span>
-                                </div>
-                            </div>
-
-                         </div>
-                        <div class="col-lg-12">
-                            <div class="button-group text-center mt-3">
-                                <a href="{{ route('business.payment-types.index') }}" class="theme-btn border-btn m-2">{{ __('Cancel') }}</a>
-                                <button class="theme-btn m-2 submit-btn">{{ __('Save') }}</button>
-                            </div>
+            <div class="modal-body p-4">
+                <form action="" method="post" enctype="multipart/form-data"
+                    class="ajaxform_instant_reload paymentTypeUpdateForm">
+                    @csrf
+                    @method('put')
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <label class="form-label fw-semibold">{{ __('Name') }}</label>
+                            <input type="text" name="name" id="PaymentTypeName" required class="form-control form-control-lg"
+                                placeholder="{{ __('Enter Name') }}">
                         </div>
-                    </form>
-                </div>
+                        <div class="col-12">
+                            <label class="form-label fw-semibold">{{ __('Status') }}</label>
+                            <select name="status" id="PaymentTypeStatus" required class="form-select form-select-lg">
+                                <option value="1">{{ __('Active') }}</option>
+                                <option value="0">{{ __('Deactive') }}</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end gap-2 mt-4">
+                        <a href="{{ route('business.payment-types.index') }}"
+                            class="btn btn-outline-secondary rounded-pill px-4">{{ __('Cancel') }}</a>
+                        <button class="btn btn-primary rounded-pill px-4 submit-btn"><i
+                                class="ri-save-3-line me-1"></i> {{ __('Save') }}</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
