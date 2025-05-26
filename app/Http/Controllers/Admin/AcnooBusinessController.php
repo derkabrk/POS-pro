370d5cc1dc522ac9f7dbda178b5725d74abd553a
rostomy <rostomyhs@gmail.com>
2025-05-26 23:37:09 +0100

    fixed business list

--- app/Http/Controllers/Admin/AcnooBusinessController.php
+++ app/Http/Controllers/Admin/AcnooBusinessController.php
@@ -133,7 +133,7 @@ class AcnooBusinessController extends Controller
                 'pictureUrl' => $request->pictureUrl ? $this->upload($request, 'pictureUrl') : NULL,
                 'user_id' => $user->id,
                 'type'=>  $request->type,
-            });
+            ]);
 
             $vat = Vat::create([
                 'name' => "Inital",
@@ -157,7 +157,7 @@ class AcnooBusinessController extends Controller
             $newUserData = [
                 'business_id'=>$business->id,
                 'name' => $request->companyName,
-                'email' =>  $request->email,
+                'email' =>  $request->email
                 'phone' => $request->phoneNumber,
                 'image' => $request->pictureUrl ? $this->upload($request, 'pictureUrl') : NULL,
                 'password' => Hash::make($request->password),
