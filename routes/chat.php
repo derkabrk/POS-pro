27a41deece0d96ae80da96a50f5edc6871573d02
rostomy <rostomyhs@gmail.com>
2025-06-02 13:30:28 +0100

    permission for super admin

--- routes/chat.php
+++ routes/chat.php
@@ -1,16 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Business\App\Http\Controllers as Business;
+use App\Http\Controllers\ChatController;
 
 // Centralized Chat Routes
 Route::middleware(['auth', 'plan_permission:chat_access'])->group(function () {
-    Route::get('chat', [Business\ChatController::class, 'index'])->name('chat.index');
-    Route::get('chat/messages/{userId}', [Business\ChatController::class, 'fetchMessages'])->name('chat.messages');
-    Route::post('chat/send', [Business\ChatController::class, 'sendMessage'])->name('chat.send');
+    Route::get('chat', [ChatController::class, 'index'])->name('chat.index');
+    Route::get('chat/messages/{userId}', [ChatController::class, 'fetchMessages'])->name('chat.messages');
+    Route::post('chat/send', [ChatController::class, 'sendMessage'])->name('chat.send');
     Route::get('chat/users/status', function() {
         $users = \App\Models\User::select('id', 'is_online')->get();
         return response()->json($users);
     })->name('chat.users.status');
-    Route::get('chat/search-users', [Business\ChatController::class, 'searchUsers'])->name('chat.search-users');
+    Route::get('chat/search-users', [ChatController::class, 'searchUsers'])->name('chat.search-users');
 });
